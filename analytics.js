6168413711752170f3f07bdfe00ae324121274e2
Nur Andi Setiabudi <nurandi.mail@gmail.com>
2026-01-30 22:04:09 +0700

    Completely externalize Google Analytics loading into analytics.js

--- analytics.js
+++ analytics.js
@@ -1,5 +1,17 @@
 // Google Analytics Configuration
-window.dataLayer = window.dataLayer || [];
-function gtag() { dataLayer.push(arguments); }
-gtag('js', new Date());
-gtag('config', 'G-2C4L8V8B29');
+(function () {
+    const gaId = 'G-2C4L8V8B29';
+
+    // Load GTAG script dynamically
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${gaId}`;
+    document.head.appendChild(script);
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    window.gtag = gtag; // Make it globally accessible if needed
+
+    gtag('js', new Date());
+    gtag('config', gaId);
+})();
