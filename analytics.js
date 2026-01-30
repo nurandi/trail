ee9633ea026be878d3efcb2131e3910ccbfc474a
Nur Andi Setiabudi <nurandi.mail@gmail.com>
2026-01-30 22:02:07 +0700

    Refactor Google Analytics into separate analytics.js file

---
+++ analytics.js
@@ -0,0 +1,5 @@
+// Google Analytics Configuration
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+gtag('config', 'G-2C4L8V8B29');
