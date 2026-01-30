934f366690ce576ec0b6daf77ce9cfcf98483b99
Nur Andi Setiabudi <nurandi.mail@gmail.com>
2026-01-30 20:10:52 +0700

    Redesign action buttons: soft rounded rectangles with icons and labels

--- app.js
+++ app.js
@@ -285,11 +285,13 @@
                     </div>
                 </div>
                 <div class="route-actions">
-                    <a href="${stravaUrl}" target="_blank" rel="noopener" class="btn-icon" title="View on Strava">
-                        <svg viewBox="-64 0 512 512"><path d="M158.4 0L7 355.7h123.7l30.1-66.7L211.2 355.7h123.8L158.4 0zm176.6 355.7l-25.3 56.4 25.3 56.4h90.6l-25.3-56.4 25.3-56.4h-90.6z"/></svg>
+                    <a href="${stravaUrl}" target="_blank" rel="noopener" class="btn-action strava" title="View on Strava">
+                        <svg viewBox="0 0 24 24"><path d="M15.387 17.944l-2.089-4.116h-3.065L15.387 24l5.15-10.172h-3.066m-7.008-4.433L2 14.331l5.15 10.171h3.066l-5.15-10.171h3.065l2.089 4.116"/></svg>
+                        <span>Strava</span>
                     </a>
-                    <button class="btn-icon primary" onclick="downloadGPX('${route.stravaId}', '${route.name}')" title="Download GPX">
+                    <button class="btn-action primary" onclick="downloadGPX('${route.stravaId}', '${route.name}')" title="Download GPX">
                         <svg viewBox="0 0 24 24"><path d="M19 9h-4V3H9v6H5l7 7 7-7zM5 18v2h14v-2H5z"></path></svg>
+                        <span>GPX</span>
                     </button>
                 </div>
             </div>
