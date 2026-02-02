6f18e6bec3bbcc134ed85bfad9b97a04ae2bcaff
Nur Andi Setiabudi <nurandi.mail@gmail.com>
2026-02-02 14:53:30 +0700

    style: replace gift emoji with FontAwesome gift icon for better hover interaction

--- app.js
+++ app.js
@@ -354,12 +354,12 @@
             if (athleteUrl && route.isPublic) {
                 displayTitle = `
                     <a href="${athleteUrl}" target="_blank" rel="noopener" class="creator-icon-link has-tooltip" data-tooltip="${tooltipText}">
-                        🎁
+                        <i class="fas fa-gift"></i>
                     </a> ${displayTitle}`;
             } else {
                 displayTitle = `
                     <span class="creator-icon-static has-tooltip" data-tooltip="${tooltipText}">
-                        🎁
+                        <i class="fas fa-gift"></i>
                     </span> ${displayTitle}`;
             }
         }
